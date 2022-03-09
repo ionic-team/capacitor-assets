f7ae5bb71708b6f2e9d911b48bb92b1d44c9cb0d
Max Lynch <max@ionic.io>
2022-03-09 16:49:59 -0600

    Add basic pwa manifest test

---
+++ test/platforms/pwa.manifest.test.ts
@@ -0,0 +1,32 @@
+import { copy, pathExists, rm } from '@ionic/utils-fs';
+import tempy from 'tempy';
+
+import { Context, loadContext } from '../../src/ctx';
+import { PwaAssetGenerator } from '../../src/platforms/pwa';
+import { AssetKind } from '../../src/definitions';
+import * as PwaAssets from '../../src/platforms/pwa/assets';
+import sharp from 'sharp';
+import { parse } from 'path';
+
+describe('PWA Manifest Test', () => {
+  let ctx: Context;
+  const fixtureDir = tempy.directory();
+
+  beforeAll(async () => {
+    await copy('test/fixtures/pwa-with-manifest', fixtureDir);
+  });
+
+  beforeEach(async () => {
+    ctx = await loadContext(fixtureDir);
+  });
+
+  afterAll(async () => {
+    await rm(fixtureDir, { force: true, recursive: true });
+  });
+
+  it('Should load PWA manifest', async () => {
+    const strategy = new PwaAssetGenerator();
+    const manifest = await strategy.getManifestJson(ctx.project);
+    expect(manifest.name).toBe('TestApp');
+  });
+});
